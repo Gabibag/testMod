93ed7a0cc84da49b1cb480770d013c244009a300
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-11-12 19:34:48 -0800

    other inits

---
+++ src/main/java/gabibag/modid/UpwardsTNT.java
@@ -0,0 +1,10 @@
+package gabibag.modid;
+
+import net.minecraft.block.Block;
+
+public class UpwardsTNT extends Block {
+
+    public UpwardsTNT(Settings settings) {
+        super(settings);
+    }
+}
